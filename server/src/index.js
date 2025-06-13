e514c35bee6772543af325d633851a887a3739e9
ElGalileo12 <kevingalileo100@gmail.com>
2025-06-13 18:54:18 -0500

    feat: se configura la conexión a la base de datos PostgreSQL

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,13 @@
 import app from "./app.js";
+import { pool } from "./config/db.js";
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor backend corriendo en http://localhost:${PORT}`);
 });
+
+pool
+  .connect()
+  .then(() => console.log("✅ Conectado a PostgreSQL"))
+  .catch((err) => console.error("❌ Error al conectar con PostgreSQL:", err));
